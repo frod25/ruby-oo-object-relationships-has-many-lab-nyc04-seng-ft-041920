5add3c5a7d825742a3a0917df3da361b7254e3df
frod25 <frod25>
2020-05-13 22:26:05 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+    attr_accessor :artist, :name
+   
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def artist=(artist)
+        @artist = artist
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        self.artist ? self.artist.name : nil
+    end
+end
